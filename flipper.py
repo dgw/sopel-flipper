e3284a139a4d33fe927dad225579d0ed22989c27
dgw <dgw@technobabbl.es>
2019-02-25 02:14:07 -0600

    Simplify code flow
    No more unnecessary temporary variables.

--- flipper.py
+++ flipper.py
@@ -19,11 +19,7 @@ def roll(bot, trigger):
     target = trigger.group(1).strip()
     if target.endswith(' down a hill'):
         target = target[:-12]
-        hill = True
-    else:
-        hill = False
-    tegrat = transform(target)
-    if hill:
+        tegrat = transform(target)
         bot.say("(╮°-°)╯︵ %s %s %s %s %s (@_@;)" % (tegrat, target, tegrat, target, tegrat))
     else:
-        bot.say("(╮°-°)╯︵ %s" % tegrat)
+        bot.say("(╮°-°)╯︵ %s" % transform(target))
