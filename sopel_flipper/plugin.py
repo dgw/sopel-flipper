2d511d70c720c6e4bcf814daf89c18ba65bad781
dgw <dgw@technobabbl.es>
2024-09-10 08:59:45 -0500

    Implement horizontal flip (mirror) action

--- sopel_flipper/plugin.py
+++ sopel_flipper/plugin.py
@@ -4,16 +4,18 @@ A Sopel plugin that flips text in response to CTCP ACTIONs.
 """
 from __future__ import annotations
 
-from upsidedown import transform
+from upsidedown import transform as flip_transform
 
 from sopel import plugin, tools
+
+from .mirror import transform as mirror_transform
 
 
 LOGGER = tools.get_logger('flipper')
 
 
-@plugin.commands('flip', 'roll')
-@plugin.action_commands('flips', 'rolls')
+@plugin.commands('flip', 'roll', 'hflip', 'mirror')
+@plugin.action_commands('flips', 'rolls', 'mirrors')
 def flip_and_roll(bot, trigger):
     target = (trigger.group(2) or '').strip()
     if not target:
@@ -30,7 +32,7 @@ def flip_and_roll(bot, trigger):
         if target in ['a table', 'the table']:
             target = '┻━┻'
         else:
-            target = transform(target)
+            target = flip_transform(target)
 
         bot.say("(╯°□°）╯︵ " + target)
     elif mode == 'roll':
@@ -40,8 +42,10 @@ def flip_and_roll(bot, trigger):
             # (`str.removesuffix()` was added in py3.9)
             target = target[:-12]
 
-        tegrat = transform(target)
+        tegrat = flip_transform(target)
         bot.say("(╮°-°)╯︵ %s %s %s %s %s (@_@;)" % (tegrat, target, tegrat, target, tegrat))
+    elif mode in ('hflip', 'mirror'):
+        bot.say("╰( ⁰ ਊ ⁰ )━💨🪞 %s" % mirror_transform(target))
     else:
         # panic!
         LOGGER.error(
